6d54a79003272c059e45a956ed2a12208bfd80b1
Biju Borah <biju20_ug@ee.nits.ac.in>
2023-06-18 03:12:12 +0530

    db connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var apiRoute = require('./routes/api')
+var mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
@@ -8,6 +9,13 @@ app.use('/api', apiRoute)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+mongoose.connect('mongodb+srv://biju:bijuborahkvcut@cluster0.q8aelky.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('Connected to DB')
 })
 
 app.listen(port, () => {
